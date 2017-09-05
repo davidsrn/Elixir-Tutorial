44fb28355ee3b360257cf6b1d1a07b283fd3eb8e
David Ramirez <davidsrnfire@gmail.com>
2017-09-05 18:17:40 -0500

    Finished Edit Auth Module

--- discuss/web/router.ex
+++ discuss/web/router.ex
@@ -7,6 +7,7 @@ defmodule Discuss.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Discuss.Plugs.SetUser
   end
 
   pipeline :api do
@@ -29,7 +30,7 @@ defmodule Discuss.Router do
   scope "/auth", Discuss do
     pipe_through :browser
 
-    get "/:provider", AuthController, :requests
+    get "/:provider", AuthController, :request#s
     get "/:provider/callback", AuthController, :callback
   end
 
